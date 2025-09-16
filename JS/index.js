ae4239fc4f68a641dff08dbae078ba7439f1aeca
Nic0lAsJoffr3 <nicolasmuitolegal@gmail.com>
2025-09-16 01:18:05 -0300

    Organizando I

--- JS/index.js
+++ JS/index.js
@@ -1,47 +1,72 @@
-import {  db, ref, push, set, onValue, get, remove, update, onDisconnect } from "./System.js";
+// importa conexão com banco de dados //
+import { db, ref, push, set, onValue, get, remove, update, onDisconnect } from "./System.js";
+
+//-----Var-----//
+
+// let //
 let pontosGravados = -1;
 let JogadorOnline = false;
+let tempoRef;
+let tempoDaPergunta;
+let Tempo = 0;
+let RespostasID = -1;
+let FimDeTempo = false;
+let jogoRef = ref(db, "Host");
+let respostaRef = ref(db, "Respostas");
+
+// Atualizar dados caso tenha mudanças no "Host" //
+
+onValue(jogoRef, (snapshot) => {
+    const dados = snapshot.val();
+    atualizarJogo(dados);
+});
+
+// Loop Geral //
+
 setInterval(() => {
-    if (localStorage.getItem("RespontaID") == null) localStorage.setItem("RespostaID", -1);
-    const jogadorRefKey = localStorage.getItem('jogadorRefKey');
+    // Cria localStorage que não existem //
+    if (localStorage.getItem("RespostaID") == null) localStorage.setItem("RespostaID", -1);
     if (localStorage.getItem("RespostaDoJogador") == null) localStorage.setItem("RespostaDoJogador", -1);
-    const jogadorRef = ref(db, `Jogadores/${jogadorRefKey}`);
-    const RespontaIDLocal = localStorage.getItem("RespontaID").toString();
+
+    // Const //
+
+    const jogadorRefKey = localStorage.getItem('jogadorRefKey');
+    const RespostaIDLocal = localStorage.getItem("RespostaID").toString();
+    const RespostaDoJogador = localStorage.getItem("RespostaDoJogador");
+    const Valor = Number(localStorage.getItem("ValorDaRespostaAtual"));
+
+    if (!jogadorRefKey) return;
+
+    const jogadorRef = ref(db, `Jogadores/${jogadorRefKey}/`);
+    const jogadorRefPerguntas = ref(db, `Jogadores/${jogadorRefKey}/perguntas/`);
+
     get(jogadorRef).then(snapshot => {
         if (!snapshot.exists()) {
             // Nó removido por outra aba
             localStorage.removeItem('nomeJogador');
             localStorage.removeItem('jogadorRefKey');
-
             document.getElementById('off').style.display = 'none';
             document.querySelector('.Entrar').style.display = 'block';
             document.querySelector('.Main').style.display = 'none';
             document.querySelector(".Time").style.display = 'none';
-
             console.log("Jogador desconectado automaticamente (outra aba fechou)");
             JogadorOnline = false;
         }
         else {
             const dados = snapshot.val();
-            const RespostaDoJogador = localStorage.getItem("RespostaDoJogador");
-
-            const jogadorRef = ref(db, `Jogadores/${jogadorRefKey}/perguntas/`);
-            if (!jogadorRefKey) return;
-            if (dados.perguntas[RespontaIDLocal] == -1) {
+            if (dados.perguntas[RespostaIDLocal] == -1) {
                 if (RespostaDoJogador != -1 && !FimDeTempo) {
-                    update(jogadorRef, {
-                        [RespontaIDLocal]: RespostaDoJogador
+                    update(jogadorRefPerguntas, {
+                        [RespostaIDLocal]: RespostaDoJogador
                     })
                 }
             }
             if (!FimDeTempo) {
-                const Valor = localStorage.getItem("ValorDaRespostaAtual");
                 if (pontosGravados <= 0) {
-                    pontosGravados = RespostaDoJogador == localStorage.getItem("RespostaCorreta") ? ((Tempo * 123) * Valor) : 0;
+                    pontosGravados = RespostaDoJogador == localStorage.getItem("RespostaCorreta") ? ((Math.max(Tempo, 0) * 123) * Valor) : 0;
                 }
             }
             else if (pontosGravados > 0) {
-                const jogadorRef = ref(db, `Jogadores/${jogadorRefKey}/`);
                 console.log(`O jogador Alcançou a pontuação de ${pontosGravados} este jogo.`)
                 pontosGravados += dados.pontos;
                 update(jogadorRef, {
@@ -52,7 +77,87 @@ setInterval(() => {
             }
         }
     });
+    if (RespostasID == -1) return;
+    get(respostaRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            const dados = snapshot.val();
+            tempoDaPergunta = dados[RespostasID].Time;
+            localStorage.setItem("RespostaCorreta", dados[RespostasID].Resposta)
+            localStorage.setItem("ValorDaRespostaAtual", dados[RespostasID].Valor)
+        }
+    });
+    if (tempoRef > 1 && tempoDaPergunta > 0) {
+        Tempo -= tempoRef - Date.now() + tempoDaPergunta * 1000;
+        Tempo /= 1000;
+        Tempo = Math.max(0, -Math.ceil(Tempo)); 
+    }
+    else return;
+    if (Tempo >= 0) {
+        FimDeTempo = false;
+        const minutos = Math.floor(Tempo / 60);
+        const segundos = Tempo % 60;
+        const textoTime = (minutos > 9 ? minutos : "0" + minutos) + ":" + (segundos > 9 ? segundos : "0" + segundos);
+        document.getElementById("Time").innerText = textoTime;
+    }
+    else if (!FimDeTempo) {
+        document.querySelector('.Main').innerHTML = `<iframe class="IPergunta" src="./Perguntas/${RespostasID}.html?type=PlayerEnd"></iframe>`;
+        FimDeTempo = true;
+    }
 }, 1000);
+
+// Função Interna //
+
+function atualizarJogo(dados) {
+    document.getElementById("TituloInicial").style.display = "block";
+    document.querySelector(".JogoStartdontgo").innerHTML = "";
+    document.querySelector(".Main").innerHTML = "";
+    document.getElementById("EntrarArea").style.display = "block";
+    if (!(dados && dados.iniciado)) {
+        // Sai do jogo e remove do DB
+        SairDoJogo();
+        document.querySelector(".ForaDoAr").style.display = 'block';
+        document.querySelector(".Every").style.display = 'none';
+        document.querySelector(".Time").style.display = 'none';
+        return;
+    }
+
+    tempoRef = dados.Time;
+    // Caso esteja logado:
+    document.querySelector(".ForaDoAr").style.display = 'none';
+    document.querySelector(".Every").style.display = 'block';
+    document.querySelector('.Main').style.display = 'block';
+
+    document.querySelector('.Main').innerHTML = `<div class="EsperandoDiv"><h1>Esperando a Partida Começar</h1></div>`;
+    if (!JogadorOnline) {
+        if (dados.PerguntasStart) {
+            document.getElementById('EntrarArea').style.display = 'none';
+            document.querySelector('.JogoStartdontgo').innerHTML = `<h1>Jogo já iniciado!</h1><br><h2>Espere o host iniciar novamente!</h2>`;
+        }
+        return;
+    }
+    if (dados.PerguntasStart) {
+        document.getElementById("TituloInicial").style.display = "none";
+        document.querySelector('.Main').innerHTML = `<iframe class="IPergunta" src="./Perguntas/${dados.PerguntasID}.html?type=Player"></iframe>`;
+        document.querySelector(".Time").style.display = 'block';
+        RespostasID = dados.PerguntasID;
+        localStorage.setItem("RespostaID", dados.PerguntasID);
+
+    } else {
+        document.querySelector(".Time").style.display = 'none';
+    }
+}
+
+function Reiniciar() {
+    get(jogoRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            const dados = snapshot.val();
+            atualizarJogo(dados);
+        }
+    }).catch((erro) => console.error("Erro ao reiniciar:", erro));
+}
+
+// Funções Externas //
+
 window.addEventListener('load', () => {
     const nome = localStorage.getItem('nomeJogador');
     const jogadorRefKey = localStorage.getItem('jogadorRefKey');
@@ -121,97 +226,3 @@ window.SairDoJogo = function () {
     document.querySelector(".Main").innerHTML = "";
     location.reload();
 }
-
-let Tempo = 0, tempoRef, tempoDaPergunta;
-let RespostasID = -1;
-let FimDeTempo = false;
-let jogoRef = ref(db, "Host");
-function atualizarJogo(dados) {
-    document.getElementById("TituloInicial").style.display = "block";
-    document.querySelector(".JogoStartdontgo").innerHTML = "";
-    document.querySelector(".Main").innerHTML = "";
-    document.getElementById("EntrarArea").style.display = "block";
-    if (!(dados && dados.iniciado)) {
-        // Sai do jogo e remove do DB
-        SairDoJogo();
-        document.querySelector(".ForaDoAr").style.display = 'block';
-        document.querySelector(".Every").style.display = 'none';
-        document.querySelector(".Time").style.display = 'none';
-        return;
-    }
-
-    tempoRef = dados.Time;
-    // Caso esteja logado:
-    document.querySelector(".ForaDoAr").style.display = 'none';
-    document.querySelector(".Every").style.display = 'block';
-    document.querySelector('.Main').style.display = 'block';
-
-    document.querySelector('.Main').innerHTML = `<div class="EsperandoDiv"><h1>Esperando a Partida Começar</h1></div>`;
-    if (!JogadorOnline) {
-        if (dados.PerguntasStart) {
-            document.getElementById('EntrarArea').style.display = 'none';
-            document.querySelector('.JogoStartdontgo').innerHTML = `<h1>Jogo já iniciado!</h1><br><h2>Espere o host iniciar novamente!</h2>`;
-        }
-        return;
-    }
-    if (dados.PerguntasStart) {
-        document.getElementById("TituloInicial").style.display = "none";
-        document.querySelector('.Main').innerHTML = `<iframe class="IPergunta" src="./Perguntas/${dados.PerguntasID}.html?type=Player"></iframe>`;
-        document.querySelector(".Time").style.display = 'block';
-        RespostasID = dados.PerguntasID;
-        localStorage.setItem("RespontaID", dados.PerguntasID);
-
-    } else {
-        document.querySelector(".Time").style.display = 'none';
-    }
-}
-
-// Escuta em tempo real
-onValue(jogoRef, (snapshot) => {
-    const dados = snapshot.val();
-    atualizarJogo(dados);
-});
-
-// Função de reiniciar
-function Reiniciar() {
-    // Pega os dados atuais **uma vez** e aplica a mesma lógica
-    get(jogoRef).then((snapshot) => {
-        if (snapshot.exists()) {
-            const dados = snapshot.val();
-            atualizarJogo(dados);
-        }
-    }).catch((erro) => console.error("Erro ao reiniciar:", erro));
-
-}
-
-setInterval(() => {
-
-    if (RespostasID == -1) return;
-
-    let respostaRef = ref(db, "Respostas");
-    get(respostaRef).then((snapshot) => {
-        if (snapshot.exists()) {
-            let dados = snapshot.val();
-            tempoDaPergunta = dados[RespostasID].Time;
-            localStorage.setItem("RespostaCorreta", dados[RespostasID].Resposta)
-            localStorage.setItem("ValorDaRespostaAtual", dados[RespostasID].Valor)
-        }
-    });
-    if (tempoRef > 1 && tempoDaPergunta > 0) {
-        Tempo -= tempoRef - Date.now() + tempoDaPergunta * 1000;
-        Tempo /= 1000;
-        Tempo = -Math.ceil(Tempo);
-    }
-    else return;
-    if (Tempo >= 0) {
-        FimDeTempo = false;
-        const minutos = Math.floor(Tempo / 60);
-        const segundos = Tempo % 60;
-        const textoTime = (minutos > 9 ? minutos : "0" + minutos) + ":" + (segundos > 9 ? segundos : "0" + segundos);
-        document.getElementById("Time").innerText = textoTime;
-    }
-    else if (!FimDeTempo) {
-        document.querySelector('.Main').innerHTML = `<iframe class="IPergunta" src="./Perguntas/${RespostasID}.html?type=PlayerEnd"></iframe>`;
-        FimDeTempo = true;
-    }
-}, 1000);
